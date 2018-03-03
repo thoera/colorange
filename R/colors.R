70eda2679599b2737162c8b4b0212e25d0f922fe
thoera <erard.thomas@gmail.com>
2018-03-03 17:29:56 +0100

    Update list of colors

--- R/colors.R
+++ R/colors.R
@@ -1,3 +1,27 @@
+# Orange's color
+list_orange_colors <- c(
+  `noir` = "#000000",
+  `gris foncé` = "#595959",
+  `gris moyen` = "#8F8F8F",
+  `gris clair` = "#D6D6D6",
+  `orange` = "#FF7900",
+  `bleu foncé` = "#085EBD",
+  `bleu` = "#4BB4E6",
+  `bleu clair` = "#B5E8F7",
+  `vert foncé` = "#0A6E31",
+  `vert` = "#50BE87",
+  `vert clair` = "#B8EBD6",
+  `rose foncé` = "#FF8AD4",
+  `rose` = "#FFB4E6",
+  `rose clair` = "#FFE8F7",
+  `violet foncé` = "#492191",
+  `violet` = "#A885D8",
+  `violet clair` = "#D9C2F0",
+  `jaune foncé` = "#FFB400",
+  `jaune` = "#FFD200",
+  `jaune clair` = "#FFF6B6"
+)
+
 #' Extract Orange's colors
 #'
 #' Extract Orange's colors as hexadecimal codes.
